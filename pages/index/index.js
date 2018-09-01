96f4faa8fc9d7a72c7e338705dd87834a8756d52
DUDU <530893915@qq.com>
2018-09-01 11:23:14 +0800

    Add PullDownRefresh

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,17 +13,52 @@ const typeMap = {
 Page({
   data: {
     newsType:['国内','国际','财经','娱乐','军事','体育','其他'],
-    newsList:[0,1,2,3,4,5,6,7,8]
+    newsList:'',
+    newsDate:[],
+    type:'gj'
   },
   onLoad() {
-    // wx.request({
-    //   url: 'https://test-miniprogram.com/api/news/list',
-    //   data: {
-        
-    //   },
-    //   success: res => {
-        
-    //   }
-    // })
-  } 
+    this.getNewsList()
+  },
+  onPullDownRefresh(){
+    this.getNewsList(() => {
+      wx.stopPullDownRefresh()
+    })
+  },
+  // 获取指定新闻类别的列表
+  getNewsList(callback){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: this.data.type
+      },
+      success: res => {
+        let result = res.data.result
+        console.log(res)
+        let realResult = []
+        for (let i = 0; i < 9; i++) {
+          realResult.push({
+            id: result[i].id,
+            title: result[i].title,
+            date: result[i].date.slice(0, 10) + " " + result[i].date.slice(11, 16),
+            source: result[i].source,
+            firstImage: result[i].firstImage
+          })
+          if (realResult[i].source === "") {
+            realResult[i].source = "DUDU新闻"
+          }
+        }
+        this.setData({
+          newsList: realResult
+        })
+      },
+      // 用回调函数来判断是否停止下拉刷新
+      complete: () => {
+
+        // 当callback不为空的时候，执行callback函数
+        callback && callback()
+
+      }
+    })
+  }
 })
